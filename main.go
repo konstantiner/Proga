77bb10200b0b8dc56a2961ef03798e9fee8be9b1
konstantiner <kastyan0403@mail.ru>
2022-06-21 19:37:05 +0300

    Редактирование
    Редактирование. Изменение данных в БД

--- main.go
+++ main.go
@@ -19,7 +19,6 @@ type Items struct {
 var posts = []Items{}
 var showItems = Items{}
 
-
 func main()  {
   handleFunc()
 }
@@ -67,10 +66,67 @@ func handleFunc()  {
   rtr.HandleFunc("/save_item", save_items).Methods("POST")
   rtr.HandleFunc("/show_item/{id:[0-9]+}", show_items).Methods("GET")
   rtr.HandleFunc("/delete_item/{id:[0-9]+}", delete_items)
+  rtr.HandleFunc("/edit/{id:[0-9]+}", EditItemPage).Methods("GET")
+  rtr.HandleFunc("/edit/{id:[0-9]+}", EditItemHandler).Methods("POST")
 
   http.Handle("/", rtr)
   http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
   http.ListenAndServe(":1212", nil)
+}
+
+func EditItemPage(w http.ResponseWriter, r *http.Request){
+  vars := mux.Vars(r)
+
+  db, _ := sql.Open("mysql", "inventory:inventory@tcp(127.0.0.1:3306)/inventory")
+
+  defer db.Close()
+
+  //Выбор данных по id
+  row := db.QueryRow(fmt.Sprintf("SELECT * FROM `items` WHERE `id` = '%s' ", vars["id"]))
+  prod := showItems
+  err := row.Scan(&prod.Id, &prod.Title, &prod.Anons, &prod.FullText)
+  if err!= nil {
+    http.Error(w, http.StatusText(404), http.StatusNotFound)
+  }else{
+    t, err := template.ParseFiles("templates/edit.html", "templates/header.html", "templates/footer.html")
+if err != nil{
+  fmt.Fprintf(w, err.Error())
+}
+    t.ExecuteTemplate(w, "edit", prod)
+  }
+}
+
+func EditItemHandler(w http.ResponseWriter, r *http.Request)  {
+  err := r.ParseForm()
+  if err !=nil{
+    panic(err)
+  }
+  id := r.FormValue("id")
+  title := r.FormValue("title")
+  anons := r.FormValue("anons")
+  full_text := r.FormValue("full_text")
+
+  db, err := sql.Open("mysql", "inventory:inventory@tcp(127.0.0.1:3306)/inventory")
+  if err != nil{
+    panic(err)
+  }
+  defer db.Close()
+
+//  fmt.Println(id, title, anons, full_text)
+
+  _, err = db.Exec("UPDATE items set title=?, anons=?, full_text=? WHERE id=?", title, anons, full_text, id)
+  if err != nil {
+    http.Error(w, http.StatusText(404), http.StatusNotFound)
+  }
+
+
+// update, err := db.Query(fmt.Sprintf("UPDATE `items` set `title`=%s, `anons`=%s, `full_text`=%s WHERE `id`=%s", title, anons, full_text, id))
+//  if err != nil {
+//    http.Error(w, http.StatusText(404), http.StatusNotFound)
+//  }
+//  defer update.Close()
+
+  http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func delete_items(w http.ResponseWriter, r *http.Request){
