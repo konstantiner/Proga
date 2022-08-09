b4c70c6557bc55749a3680bb7f42e70712bee0a1
konstantiner <70434849+konstantiner@users.noreply.github.com>
2022-08-09 17:38:29 +0300

    Списание_2
    Списанные в отдельной странице. Таблица с списанным

--- main.go
+++ main.go
@@ -23,11 +23,32 @@ func main() {
 	handleFunc()
 }
 
-func show_items(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
+func handleFunc() {
+	rtr := mux.NewRouter()
+	rtr.HandleFunc("/", index).Methods("GET")
+	rtr.HandleFunc("/create", create_items).Methods("GET")
+	rtr.HandleFunc("/save_item", save_items).Methods("POST")
+	rtr.HandleFunc("/show_item/{id:[0-9]+}", show_items).Methods("GET")
+	rtr.HandleFunc("/delete_item/{id:[0-9]+}", delete_items)
+	rtr.HandleFunc("/edit/{id:[0-9]+}", EditItemPage).Methods("GET")
+	rtr.HandleFunc("/edit/{id:[0-9]+}", EditItemHandler).Methods("POST")
+	rtr.HandleFunc("/write_off_item/{id:[0-9]+}", WriteOffItem).Methods("POST", "GET")
+	rtr.HandleFunc("/listwriteoff", list_writeoff).Methods("GET")
 
-	t, err := template.ParseFiles("templates/show.html", "templates/header.html", "templates/footer.html")
+	http.Handle("/", rtr)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	http.ListenAndServe(":1212", nil)
+}
+func list_writeoff(w http.ResponseWriter, r *http.Request) {
+	type list struct {
+		Id                     uint16
+		Title, Anons, FullText string
+		Data                   string
+	}
 
+	var writeoff = []list{}
+
+	t, err := template.ParseFiles("templates/listwriteoff.html", "templates/header.html", "templates/footer.html")
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
 	}
@@ -39,40 +60,24 @@ func show_items(w http.ResponseWriter, r *http.Request) {
 	defer db.Close()
 
 	//Выборка данных
-	res, err := db.Query(fmt.Sprintf("SELECT * FROM `items` WHERE `id` = '%s' ", vars["id"]))
+	res, err := db.Query("SELECT * FROM `writeoffitem` ")
 	if err != nil {
 		panic(err)
 	}
 
-	showItems = Items{} //очищаем список
+	writeoff = []list{} //очищаем список
 	for res.Next() {
-		var post Items
-		err = res.Scan(&post.Id, &post.Title, &post.Anons, &post.FullText)
+		var post list
+		err = res.Scan(&post.Id, &post.Title, &post.Anons, &post.FullText, &post.Data)
 		if err != nil {
 			panic(err)
 		}
 
-		showItems = post
+		writeoff = append(writeoff, post)
 	}
 
-	t.ExecuteTemplate(w, "show", showItems)
+	t.ExecuteTemplate(w, "listwriteoff", writeoff)
 
-}
-
-func handleFunc() {
-	rtr := mux.NewRouter()
-	rtr.HandleFunc("/", index).Methods("GET")
-	rtr.HandleFunc("/create", create_items).Methods("GET")
-	rtr.HandleFunc("/save_item", save_items).Methods("POST")
-	rtr.HandleFunc("/show_item/{id:[0-9]+}", show_items).Methods("GET")
-	rtr.HandleFunc("/delete_item/{id:[0-9]+}", delete_items)
-	rtr.HandleFunc("/edit/{id:[0-9]+}", EditItemPage).Methods("GET")
-	rtr.HandleFunc("/edit/{id:[0-9]+}", EditItemHandler).Methods("POST")
-	rtr.HandleFunc("/write_off_item/{id:[0-9]+}", WriteOffItem).Methods("POST", "GET")
-
-	http.Handle("/", rtr)
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
-	http.ListenAndServe(":1212", nil)
 }
 
 func WriteOffItem(w http.ResponseWriter, r *http.Request) { //Списание
@@ -249,4 +254,40 @@ func save_items(w http.ResponseWriter, r *http.Request) {
 
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 	}
+}
+
+func show_items(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	t, err := template.ParseFiles("templates/show.html", "templates/header.html", "templates/footer.html")
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	db, err := sql.Open("mysql", "inventory:inventory@tcp(127.0.0.1:3306)/inventory")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	//Выборка данных
+	res, err := db.Query(fmt.Sprintf("SELECT * FROM `items` WHERE `id` = '%s' ", vars["id"]))
+	if err != nil {
+		panic(err)
+	}
+
+	showItems = Items{} //очищаем список
+	for res.Next() {
+		var post Items
+		err = res.Scan(&post.Id, &post.Title, &post.Anons, &post.FullText)
+		if err != nil {
+			panic(err)
+		}
+
+		showItems = post
+	}
+
+	t.ExecuteTemplate(w, "show", showItems)
+
 }
